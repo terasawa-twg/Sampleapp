cb65807b29c6bd66e53ba0067997bb1d9e812273
t-shiho <t-shiho@techno-wing.co.jp>
2025-07-02 10:19:05 +0900

    ファイル管理のパスを'/files'から'/file-list'に変更

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -50,7 +50,7 @@ export function Sidebar({ currentUser, currentPath = "/" }: SidebarProps) {
     {
       icon: Camera,
       label: 'ファイル管理',
-      path: '/files'
+      path: '/file-list'
     },
     {
       icon: Clock,
